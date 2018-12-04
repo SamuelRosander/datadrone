3327db97c429f4663a7e47e9ffbb9cee43c144b8
Samuel Rosander <s.rosander@gmail.com>
2018-12-04 18:38:09 +0100

    CSS changes to EntryTag edit

---
+++ datadrone/static/js/entry.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("#link-show-tags").click(function() {
+    $("#link-show-tags").hide();
+    $("#link-hide-tags").show();
+    $(".tag-box-label").css("display" , "inline-block");
+    $(".tag-box-label").removeClass("tag-box-label-disable").addClass("tag-box-label-enable");
+  });
+
+  $("#link-hide-tags").click(function() {
+    $("#link-show-tags").show();
+    $("#link-hide-tags").hide();
+    $(".tag-box-label").removeClass("tag-box-label-enable").addClass("tag-box-label-disable");
+    $(".tag-label-checkbox:not(:checked) + .tag-box-label").hide();
+  });
+
+});

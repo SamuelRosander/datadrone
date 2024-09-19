cbe754124800fb8d4ce93054253d9ac48d9785b4
Samuel Rosander <s.rosander@gmail.com>
2024-09-19 19:23:34 +0200

    Change to MySQL

--- datadrone/extensions.py
+++ datadrone/extensions.py
@@ -4,6 +4,9 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail, Message
 from flask_caching import Cache
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 db = SQLAlchemy()
 login_manager = LoginManager()
